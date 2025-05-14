503f9abd185652f9d6403ff537b32a8d4f362313
Blakev542 <blake.vandoren1@gmail.com>
2025-05-14 14:57:46 +1000

    Initial commit

---
+++ app/src/main/java/com/example/oscarsapp/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.oscarsapp
+
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.oscarsapp.databinding.ActivityMainBinding
+import android.widget.Toast
+import androidx.lifecycle.ReportFragment.Companion.reportFragment
+import com.example.oscarsapp.ui.dashboard.DashboardFragment
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+
+    fun sendMoney(view: View) {
+
+        Toast.makeText(this, "hi", Toast.LENGTH_SHORT).show()
+
+    }
+}
